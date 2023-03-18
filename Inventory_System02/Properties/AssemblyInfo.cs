a3142b35ceb1b879d30826b235132174716eb5e0
eugen <eugenereybulahan@gmail.com>
2023-03-18 22:04:36 +0800

    feat: revision 3

--- Inventory_System02/Properties/AssemblyInfo.cs
+++ Inventory_System02/Properties/AssemblyInfo.cs
@@ -1,17 +1,18 @@
-﻿using System.Reflection;
+﻿using System.Resources;
+using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Inventory_System02")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("Inventory MS")]
+[assembly: AssemblyDescription("Powered By Codefilter Technology")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Inventory_System02")]
-[assembly: AssemblyCopyright("Copyright ©  2021")]
-[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCompany("Codefilter Technology")]
+[assembly: AssemblyProduct("Inventory MS")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("Codefilter Technology")]
 [assembly: AssemblyCulture("")]
 
 // Setting ComVisible to false makes the types in this assembly not visible
@@ -34,3 +35,4 @@ using System.Runtime.InteropServices;
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: NeutralResourcesLanguage("en")]
