d855c840e347dc31a73a85ec60b5b50a884c0d97
eugen <eugenereybulahan@gmail.com>
2023-03-19 11:58:31 +0800

    feat: Roll back to revision 0 stand alone

--- Inventory_System02/Includes/App_Settings.cs
+++ Inventory_System02/Includes/App_Settings.cs
@@ -19,12 +19,6 @@ namespace Inventory_System02.Includes
         //public static string Database { get; set; } = @"Data Source = CommonSql\Database.db;Version=3;New=False;Read Only = False;Compress=True;Journal Mode=Off;providerName=System.Data.SQlite;";
 
         //public static string Database { get; set; } = $"Data Source={Path.Combine(Application.StartupPath, "CommonSql", "Tools", "tools.dll")};Version=3;New=False;Read Only=False;Compress=True;Journal Mode=Off;providerName=System.Data.SQLite;";
-
-        ////using Clickonces      
-        //public  static string path = ApplicationDeployment.CurrentDeployment.DataDirectory;
-        //public static string databasePath = Path.Combine(path, "CommonSql", "Tools", "tools.dll");
-        //public static string Database = $"Data Source={databasePath};Version=3;New=False;Read Only=False;Compress=True;Journal Mode=Off;providerName=System.Data.SQLite;";
-
         //Images
         public static string app_value { get; set; } = "74C50BEC-CBFD-4B91-A5E2-AD9F3AE66319";
         public static string DateFormatRetrieve { get; set; } = "yyyy-MM-dd";
@@ -63,98 +57,27 @@ namespace Inventory_System02.Includes
        
         public static void My_path()
         {
-
-            // Check the configuration setting to determine which connection string to use
-            bool useClickOnceConnection = ConfigurationManager.AppSettings["UseClickOnceConnection"] == "false";
-
-            if (useClickOnceConnection)
-            {
-                // Update the paths for the other fields
-
-                string appDataDir = Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData);
-                string clickOnceDataDir = Path.Combine(appDataDir, "Apps", "2.0", "Data");
-
-                string[] clickOnceDirs = Directory.GetDirectories(clickOnceDataDir, "*.*/", SearchOption.TopDirectoryOnly);
-                string randomStringDir = clickOnceDirs.FirstOrDefault(d => Directory.GetDirectories(d, "*", SearchOption.TopDirectoryOnly).Length > 0);
-
-                if (randomStringDir != null)
-                {
-                    string commonSqlDir = Path.Combine(randomStringDir, "Data", "CommonSql");
-                    Image_DIR = Path.Combine(commonSqlDir, "Pictures", "Item", "Image");
-                    Customer_DIR =  Path.Combine(commonSqlDir, "Pictures", "Pictures", "Customers");
-                    Supplier_DIR = Path.Combine(commonSqlDir, "Pictures", "Suppliers");
-                    Employee_DIR = Path.Combine(commonSqlDir, "Pictures", "Employee");
-                    Company_DIR = Path.Combine(commonSqlDir, "Pictures", "Company");
-                    Doc_DIR = Path.Combine(commonSqlDir, "Document Center Files");
-                    Search_DTG = Path.Combine(commonSqlDir, "Reports Dir", "Search DTG", "Search_DTG.rdlc");
-                    Invoice_RDLC_Path = Path.Combine(commonSqlDir, "Invoice");
-                    Invoice_BY_SupDivi = Path.Combine(commonSqlDir, "Reports Dir", "Item_Division", "Item_Divisup_Report.rdlc");
-                    Supplier_RDLC_DIR = Path.Combine(commonSqlDir, "CommonSql", "Reports Dir", "Supplier_Report.rdlc");
-                    Employee_RDLC_DIR = Path.Combine(commonSqlDir, "CommonSql", "Reports Dir", "Employee_Report.rdlc");
-                    Customer_RDLC_DIR = Path.Combine(commonSqlDir, "CommonSql", "Reports Dir", "Customer_Report.rdlc");
-                    Item_RDLC_DIR = Path.Combine(commonSqlDir, "CommonSql", "Reports Dir", "Item Report.rdlc");
-                    Item_qty_RDLC_DIR = Path.Combine(commonSqlDir, "Reports Dir", "Item_Qty", "ItemQTY_Report.rdlc");
-                }
-                else
-                {
-                    throw new Exception("Unable to locate ClickOnce data directory.");
-                }
-            }
-            else
-            {
-             
-                Image_DIR = Path.Combine(Application.StartupPath, "CommonSql", "Pictures", "Item", "Image");
-                Customer_DIR = Path.Combine(Application.StartupPath, "CommonSql", "Pictures", "Customers");
-                Supplier_DIR = Path.Combine(Application.StartupPath, "CommonSql", "Pictures", "Suppliers");
-                Employee_DIR = Path.Combine(Application.StartupPath, "CommonSql", "Pictures", "Employee");
-                Company_DIR = Path.Combine(Application.StartupPath, "CommonSql", "Pictures", "Company");
-                Doc_DIR = Path.Combine(Application.StartupPath, "CommonSql", "Document Center Files");
-                Search_DTG = Path.Combine(Application.StartupPath, "CommonSql", "Reports Dir", "Search DTG", "Search_DTG.rdlc");
-                Invoice_RDLC_Path = Path.Combine(Application.StartupPath, "CommonSql", "Invoice");
-                Invoice_BY_SupDivi = Path.Combine(Application.StartupPath, "CommonSql", "Reports Dir", "Item_Division", "Item_Divisup_Report.rdlc");
-                Supplier_RDLC_DIR = Path.Combine(Application.StartupPath, "CommonSql", "Reports Dir", "Supplier_Report.rdlc");
-                Employee_RDLC_DIR = Path.Combine(Application.StartupPath, "CommonSql", "Reports Dir", "Employee_Report.rdlc");
-                Customer_RDLC_DIR = Path.Combine(Application.StartupPath, "CommonSql", "Reports Dir", "Customer_Report.rdlc");
-                Item_RDLC_DIR = Path.Combine(Application.StartupPath, "CommonSql", "Reports Dir", "Item Report.rdlc");
-                Item_qty_RDLC_DIR = Path.Combine(Application.StartupPath, "CommonSql", "Reports Dir", "Item_Qty", "ItemQTY_Report.rdlc");
-            }
-
+            Image_DIR = Path.Combine(Application.StartupPath, "CommonSql", "Pictures", "Item", "Image");
+            Customer_DIR = Path.Combine(Application.StartupPath, "CommonSql", "Pictures", "Customers");
+            Supplier_DIR = Path.Combine(Application.StartupPath, "CommonSql", "Pictures", "Suppliers");
+            Employee_DIR = Path.Combine(Application.StartupPath, "CommonSql", "Pictures", "Employee");
+            Company_DIR = Path.Combine(Application.StartupPath, "CommonSql", "Pictures", "Company");
+            Doc_DIR = Path.Combine(Application.StartupPath, "CommonSql", "Document Center Files");
+            Search_DTG = Path.Combine(Application.StartupPath, "CommonSql", "Reports Dir", "Search DTG", "Search_DTG.rdlc");
+            Invoice_RDLC_Path = Path.Combine(Application.StartupPath, "CommonSql", "Invoice");
+            Invoice_BY_SupDivi = Path.Combine(Application.StartupPath, "CommonSql", "Reports Dir", "Item_Division", "Item_Divisup_Report.rdlc");
+            Supplier_RDLC_DIR = Path.Combine(Application.StartupPath, "CommonSql", "Reports Dir", "Supplier_Report.rdlc");
+            Employee_RDLC_DIR = Path.Combine(Application.StartupPath, "CommonSql", "Reports Dir", "Employee_Report.rdlc");
+            Customer_RDLC_DIR = Path.Combine(Application.StartupPath, "CommonSql", "Reports Dir", "Customer_Report.rdlc");
+            Item_RDLC_DIR = Path.Combine(Application.StartupPath, "CommonSql", "Reports Dir", "Item Report.rdlc");
+            Item_qty_RDLC_DIR = Path.Combine(Application.StartupPath, "CommonSql", "Reports Dir", "Item_Qty", "ItemQTY_Report.rdlc");
+  
         }
 
         public static string Database()
-        {
-            string connectionString = "";
-
-            // Check the configuration setting to determine which connection string to use
-            bool useClickOnceConnection = ConfigurationManager.AppSettings["UseClickOnceConnection"] == "false";
-
-            if (useClickOnceConnection)
-            {
-
-                string appDataDir = Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData);
-                string clickOnceDataDir = Path.Combine(appDataDir, "Apps", "2.0");
-
-                string[] clickOnceDirs = Directory.GetDirectories(clickOnceDataDir, "*.*/", SearchOption.TopDirectoryOnly);
-                string randomStringDir = clickOnceDirs.FirstOrDefault(d => Directory.GetDirectories(d, "*", SearchOption.TopDirectoryOnly).Length > 0);
-
-                if (randomStringDir != null)
-                {
-                    string commonSqlDir = Path.Combine(randomStringDir, "CommonSql");
-                    string databasePath = Path.Combine(commonSqlDir, "Tools", "tools.db");
-                    MessageBox.Show(databasePath);
-                    connectionString = $"Data Source={databasePath};Version=3;New=False;Read Only=False;Compress=True;Journal Mode=Off;providerName=System.Data.SQLite;";
-                }
-                else
-                {
-                    throw new Exception("Unable to locate ClickOnce data directory.");
-                }
-                   
-            }
-            else
-            {
-                // Use the hard-coded connection string approach
-                connectionString = $"Data Source={Path.Combine(Application.StartupPath, "CommonSql", "Tools", "tools.dll")};Version=3;New=False;Read Only=False;Compress=True;Journal Mode=Off;providerName=System.Data.SQLite;";          
-            }
+        {        
+            // Use the hard-coded connection string approach
+            string connectionString = $"Data Source={Path.Combine(Application.StartupPath, "CommonSql", "Tools", "tools.dll")};Version=3;New=False;Read Only=False;Compress=True;Journal Mode=Off;providerName=System.Data.SQLite;";  
 
             return connectionString;
         }
